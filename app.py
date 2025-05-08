dc19239512d6401a203d07113827faeeae43914c
sgallaher <54886009+sgallaher@users.noreply.github.com>
2025-05-08 07:31:36 +0000

    with port

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from random import randint,choice
+import os
 app = Flask(__name__)
 @app.route('/')
 def home():
@@ -23,3 +24,6 @@ def home():
             puzzle.append([select,hints])
         
     return render_template("index.html", puzzle=puzzle)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
